9baa1b97e948c7227822ca330c85c7155bc9e1d2
GregBiglione <gregory.biglione81@gmail.com>
2023-10-24 16:25:56 +0200

    Added LoginViewModel

---
+++ lib/src/presentation/screen/auth/login/login_viewmodel.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:gamer_mvvm/src/presentation/screen/auth/login/login_state.dart';
+import 'package:gamer_mvvm/src/presentation/utils/validation_item.dart';
+
+class LoginViewModel extends ChangeNotifier {
+  LoginState _loginState = LoginState();
+
+  // Getters -------------------------------------------------------------------
+  LoginState get loginState => _loginState;
+
+  // Setters -------------------------------------------------------------------
+  void changeEmail(String value) {
+    _loginState = _loginState.copyWith(email: ValidationItem(value: value));
+    notifyListeners();
+  }
+
+  void changePassword(String value) {
+    _loginState = _loginState.copyWith(password: ValidationItem(value: value));
+    notifyListeners();
+  }
+
+  // ---------------------------------------------------------------------------
+  // Login ---------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
+
+  void login() {
+    print("Email: ${_loginState.email} \nPassword: ${_loginState.password}");
+  }
+}
