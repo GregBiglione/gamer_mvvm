0da49e4225e6c57fd426175902c151ed5fd711f9
GregBiglione <gregory.biglione81@gmail.com>
2023-10-31 19:47:18 +0100

    Added UserRepository

---
+++ lib/src/domain/repository/user_repository.dart
@@ -0,0 +1,6 @@
+import 'package:gamer_mvvm/src/domain/model/user_data.dart';
+import '../utils/resource.dart';
+
+abstract class UserRepository {
+  Stream<Resource<UserData>> getUserById(String id);
+}
