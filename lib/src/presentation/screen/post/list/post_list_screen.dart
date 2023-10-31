0e1d70d6afbdc85d7b32dd96dd57b397a803ea94
GregBiglione <gregory.biglione81@gmail.com>
2023-10-31 17:43:31 +0100

    Added PostListScreen & MyPostListScreen

---
+++ lib/src/presentation/screen/post/list/post_list_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class PostListScreen extends StatelessWidget {
+  const PostListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Post list screen"),
+      ),
+    );
+  }
+}
