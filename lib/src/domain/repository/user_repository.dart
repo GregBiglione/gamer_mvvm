9379389789776dae503cf3041c09542d0bc32fb4
GregBiglione <gregory.biglione81@gmail.com>
2023-11-02 16:53:42 +0100

    Added updateWithImage() in UserRepository

--- lib/src/domain/repository/user_repository.dart
+++ lib/src/domain/repository/user_repository.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
 import 'package:gamer_mvvm/src/domain/model/user_data.dart';
 import '../utils/resource.dart';
 
 abstract class UserRepository {
   Stream<Resource<UserData>> getUserById(String id);
   Future<Resource<String>> updateWithoutImage(UserData userData);
+  Future<Resource<String>> updateWithImage(UserData userData, File image);
 }
