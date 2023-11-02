76a2488dd2b873d9927271d39fb8292e700a709b
GregBiglione <gregory.biglione81@gmail.com>
2023-11-02 19:30:39 +0100

    Added CreatePostContent

--- lib/src/presentation/screen/post/create/create_post_screen.dart
+++ lib/src/presentation/screen/post/create/create_post_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gamer_mvvm/src/presentation/screen/post/create/widget/create_post_content.dart';
 
 class CreatePostScreen extends StatelessWidget {
   const CreatePostScreen({super.key});
@@ -6,9 +7,7 @@ class CreatePostScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text("Crete post screen"),
-      ),
+      body: CreatePostContent(),
     );
   }
 }
---
+++ lib/src/presentation/screen/post/create/widget/create_post_content.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CreatePostContent extends StatelessWidget {
+  const CreatePostContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Text("Crete post content"),
+        ],
+      ),
+    );
+  }
+}
