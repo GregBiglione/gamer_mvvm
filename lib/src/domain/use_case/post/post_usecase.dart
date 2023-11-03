a055578071b0dbcd5f78ac20db965b0fd411782d
GregBiglione <gregory.biglione81@gmail.com>
2023-11-03 22:38:12 +0100

    Added PostUseCase

---
+++ lib/src/domain/use_case/post/post_usecase.dart
@@ -0,0 +1,7 @@
+import 'package:gamer_mvvm/src/domain/use_case/post/create_post_usecase.dart';
+
+class PostUseCase {
+  final CreatePostUseCase createPostUseCase;
+
+  PostUseCase({required this.createPostUseCase});
+}
