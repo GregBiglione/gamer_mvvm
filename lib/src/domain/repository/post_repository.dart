3a2c1eb5b7c4f1807d92e371874a2ad899175dd5
GregBiglione <gregory.biglione81@gmail.com>
2023-11-03 21:57:59 +0100

    Added PostRepository

---
+++ lib/src/domain/repository/post_repository.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+import 'package:gamer_mvvm/src/domain/model/post.dart';
+import '../utils/resource.dart';
+
+abstract class PostRepository {
+  Future<Resource<String>> create(Post post, File image);
+}
