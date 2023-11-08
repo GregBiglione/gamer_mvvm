4ba1aa630c1cd5628a8bda4edd8327b831509000
GregBiglione <gregory.biglione81@gmail.com>
2023-11-08 17:48:42 +0100

    Modified MyPostListScreen

--- lib/src/presentation/screen/post/my_list/my_post_list_screen.dart
+++ lib/src/presentation/screen/post/my_list/my_post_list_screen.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:gamer_mvvm/src/presentation/screen/post/my_list/my_post_list_viewmodel.dart';
+import 'package:gamer_mvvm/src/presentation/screen/post/my_list/widget/my_post_list_item.dart';
 import 'package:gamer_mvvm/src/presentation/utils/base_color.dart';
+import 'package:provider/provider.dart';
+
+import '../../../../domain/model/post.dart';
+import '../../../../domain/utils/resource.dart';
 
 class MyPostListScreen extends StatelessWidget {
   const MyPostListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    MyPostListViewModel viewModel = Provider.of<MyPostListViewModel>(context);
+
     return Scaffold(
-      body: Center(
-        child: Text("My post list screen"),
+      body: Scaffold(
+        backgroundColor: backgroundColor,
+        body: StreamBuilder(
+          stream: viewModel.getAllPostById(),
+          builder: (context, snapshot) {
+            if(snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if(!snapshot.hasData) {
+              return const Center(
+                child: Text("No hay información"),
+              );
+            }
+
+            final response = snapshot.data;
+            if(response is Error) {
+              final data = response as Error;
+
+              return Center(
+                child: Text("Error: ${data.error}"),
+              );
+            }
+
+            final postList = response as Success<List<Post>>;
+            return ListView.builder(
+              itemCount: postList.data.length,
+              itemBuilder: (context, index) {
+                return MyPostListItem(post: postList.data[index]);
+              },
+            );
+          },
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: baseColor,
